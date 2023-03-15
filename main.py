fa8250c72fb373252364504c5102c5d8022ab291
74Per <percent741017@gmail.com>
2023-03-15 22:58:30 +0900

    :memo:GPTsuck

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
-# module import
-# python3 -m pip install openai
-
+import os
 import openai
-import json
-
-# OpenAI API 키 설정
-openai.api_key = "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"
-
-# API 요청 보내기
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="davinci", # GPT 모델의 엔진 이름
-        prompt=prompt, # 대화 시작 문장
-        max_tokens=60, # 생성할 텍스트의 최대 길이
-        n=1, # 생성할 텍스트의 개수
-        stop=None, # 생성할 텍스트의 종료 조건
-        temperature=0.7, # 다양한 출력을 생성하기 위한 온도 매개변수
-    )
-    message = response.choices[0].text.strip()
-    return message
-
-# 대화 생성 예제
+openai.api_key = "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"
+messages=[]
 while True:
-    prompt = input("You: ")
-    prompt = prompt[-1]
-    prompt = "'" + prompt + "'"
-    prompt += "로 시작하는 2글자 단어 하나 알려줘"
-    response = generate_text(prompt)
-    print("AI: " + response)
+    user_content = input("user : ")
+
+    messages.append({"role" : "user", "content" : f"{user_content}"})
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    assistant_content = completion.choices[0].message["content"].strip()
+    messages.append({"role" : "user", "content" : f"{user_content}"})
+    print(f"GPT : {assistant_content}")
