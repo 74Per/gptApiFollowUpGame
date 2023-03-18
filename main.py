eaaf5c37ba459dc0f7be24e14a87d13a2f0ae924
74Per <percent741017@gmail.com>
2023-03-18 20:29:17 +0900

    :wastebasket:GPTtrash

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import openai
-openai.api_key = "API키 뽑아서 쓰기"
+openai.api_key = "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"
 messages=[]
 while True:
     user_content = input("user : ")
-
+    user_content = user_content[-1]
+    print(user_content)
+    user_content+="로 시작하는 단어 하나 말해줘"
     messages.append({"role" : "user", "content" : f"{user_content}"})
 
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
